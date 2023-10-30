16aa84034ca09c0892deb1588ad40833256fb159
ApointO <antonstengaardolsen@gmail.com>
2023-10-30 05:49:57 +0100

    Update form_overview.dart

--- lib/screens/add_fountain/form_overview.dart
+++ lib/screens/add_fountain/form_overview.dart
@@ -1,8 +1,16 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:toerst/models/fountain.dart';
 import 'package:geocoding/geocoding.dart'; // Import the geocoding package
+import 'package:toerst/widgets/standard_button.dart';
+
+// Declare constants for easy adjustments
+const double imageScaleFactor = 1.35;
+const double imagePadding = 22.0;
+const double containerInnerPadding = 8.0;
+const double cardOuterPadding = 16.0;
+const double buttonBottomPadding = 40.0;
+const double containerBottomPadding = 16.0;
 
 class FormOverview extends StatefulWidget {
   final Fountain fountainData;
@@ -54,43 +62,102 @@ class _FormOverviewState extends State<FormOverview> {
     }
   }
 
+  Widget _buildStarRating(int rating) {
+    return Row(
+      children: List.generate(
+        rating,
+        (index) => Icon(
+          Icons.star,
+          color: Colors.black,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.white,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
             Navigator.pop(context);
           },
         ),
-        title: const Text('Review Your Entry'),
+        title: const Text(
+          'Review Your Entry',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            if (_decodeImage(widget.fountainData.imageBase64Format) != null)
-              _decodeImage(widget.fountainData.imageBase64Format)!,
-            const SizedBox(height: 20),
-            Text('Type: ${widget.fountainData.type}'),
-            const SizedBox(height: 10),
-            Text('Rating: ${widget.fountainData.rating}'),
-            const SizedBox(height: 10),
-            Text('Address: ${_address ?? 'Could not fetch address.'}'),
-            const SizedBox(height: 10),
-            Text(
-                'Review: ${widget.fountainData.review ?? 'No review provided.'}'),
-            const SizedBox(height: 20),
-            ElevatedButton(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16.0),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  elevation: 5,
+                  child: Padding(
+                    padding: const EdgeInsets.all(cardOuterPadding),
+                    child: Padding(
+                      padding: const EdgeInsets.all(containerInnerPadding),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(imagePadding),
+                            child: Align(
+                              alignment: Alignment.topCenter,
+                              child: Transform.scale(
+                                scale: imageScaleFactor,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  child: _decodeImage(widget
+                                          .fountainData.imageBase64Format) ??
+                                      Container(),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Text('${widget.fountainData.type}'),
+                          const SizedBox(height: 10),
+                          _buildStarRating(
+                              (widget.fountainData.rating ?? 0).toInt()),
+                          const SizedBox(height: 10),
+                          Text('${_address ?? 'Could not fetch address.'}'),
+                          const SizedBox(height: 10),
+                          Text(
+                              '${widget.fountainData.review ?? 'No review provided.'}'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: buttonBottomPadding),
+            child: StandardButton(
+              label: 'Submit',
               onPressed: () {
                 // Submit data to the backend
               },
-              child: const Text('Submit'),
+              backgroundColor: Colors.blue,
+              textColor: Colors.white,
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+              height: containerBottomPadding), // Add some space at the bottom
+        ],
       ),
     );
   }
